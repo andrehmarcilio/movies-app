e955ffc4fc8c78c6c9df29ca23ba15171447c3c0
Andre Marcilio <andremarcilio@jera.com.br>
2023-04-30 00:12:48 -0300

    [STYLE] added app logo in assets

---
+++ lib/support/style/app_assets.dart
@@ -0,0 +1,6 @@
+abstract class AppAssets {
+  static const _assetsPath = 'assets';
+  static const _imagesPath = '$_assetsPath/images';
+
+  static const appLogo = '$_imagesPath/movies_app_logo.png';
+}
