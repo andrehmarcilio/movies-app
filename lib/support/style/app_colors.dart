442c25163273220f3fe89eafcfcf89488ae732d2
Andre Marcilio <andremarcilio@jera.com.br>
2023-05-01 00:51:55 -0300

    [STYLE] updated app theme

--- lib/support/style/app_colors.dart
+++ lib/support/style/app_colors.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 abstract class AppColors {
   static const Color purple = Color.fromRGBO(40, 6, 99, 1);
   static const Color deepPurple = Color.fromRGBO(16, 2, 40, 1);
-  static const Color lightPurple = Color.fromRGBO(246, 244, 251, 1);
+
   static const Color black = Color.fromRGBO(20, 20, 20, 1);
+  static const Color white = Color.fromARGB(255, 245, 240, 240);
 }
