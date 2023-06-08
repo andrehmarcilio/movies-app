df8f0dbffd88241060e3140acee8098942507922
Andre Marcilio <andrehmarcilio@gmail.com>
2023-06-08 19:28:35 -0300

    [UPDATE] added search feature dependencies in injector class

--- lib/support/injector/app_injector.dart
+++ lib/support/injector/app_injector.dart
@@ -2,9 +2,11 @@ import 'package:get_it/get_it.dart';
 
 import '../../data/remote/routes/movies_routes.dart';
 import '../../data/remote/setup/http_client.dart';
-import '../../features/dashboard/movies_carousel/cubit/movies_carousel_cubit.dart';
-import '../../features/dashboard/movies_horizontal_list/cubit/movies_horizontal_list_cubit.dart';
+import '../../features/dashboard/movies_carousel/state/movies_carousel_view_model.dart';
+import '../../features/dashboard/movies_horizontal_list/state/movies_horizontal_list_view_model.dart';
 import '../../features/dashboard/use_cases/get_movies_use_case.dart';
+import '../../features/search/state/search_movies_view_model.dart';
+import '../../features/search/use_cases/search_movies_use_case.dart';
 
 class AppInjector {
   static final _provider = GetIt.instance;
@@ -21,17 +23,26 @@ class AppInjector {
 
     // Common Singletons
     _provider.registerSingleton<HttpClient>(DioClient());
-    _provider.registerSingleton<MoviesRoutes>(MoviesRoutesImpl(get<HttpClient>()));
+
+    _provider.registerSingleton<MoviesRoutes>(MoviesRoutesImpl(_provider.get<HttpClient>()));
 
     // Dashboard Feature
     _provider.registerFactory<GetMoviesUseCase>(() {
-      return GetMoviesUseCaseImpl(moviesRoutes: get<MoviesRoutes>());
+      return GetMoviesUseCaseImpl(moviesRoutes: _provider.get<MoviesRoutes>());
     });
-    _provider.registerFactory<MoviesHorizontalListCubit>(() {
-      return MoviesHorizontalListCubit(getMoviesUseCase: get<GetMoviesUseCase>());
+    _provider.registerFactory<MoviesHorizontalListViewModel>(() {
+      return MoviesHorizontalListViewModel(getMoviesUseCase: _provider.get<GetMoviesUseCase>());
     });
-    _provider.registerFactory<MoviesCarouselCubit>(() {
-      return MoviesCarouselCubit(getMoviesUseCase: get<GetMoviesUseCase>());
+    _provider.registerFactory<MoviesCarouselViewModel>(() {
+      return MoviesCarouselViewModel(getMoviesUseCase: _provider.get<GetMoviesUseCase>());
+    });
+
+    // Search Feature
+    _provider.registerFactory<SearchMoviesUseCase>(() {
+      return SearchMoviesUseCaseImpl(moviesRoutes: _provider.get<MoviesRoutes>());
+    });
+    _provider.registerFactory<SearchMoviesViewModel>(() {
+      return SearchMoviesViewModel(searchMoviesUseCase: _provider.get<SearchMoviesUseCase>());
     });
   }
 }
