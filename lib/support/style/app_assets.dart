4d986694a59109d15913e03d58631e802aea0a40
Andre Marcilio <andrehmarcilio@gmail.com>
2023-06-09 12:11:32 -0300

    [NEW] added new icons

--- lib/support/style/app_assets.dart
+++ lib/support/style/app_assets.dart
@@ -1,6 +1,10 @@
 abstract class AppAssets {
   static const _assetsPath = 'assets';
   static const _imagesPath = '$_assetsPath/images';
+  static const _iconsPath = '$_assetsPath/icons';
 
   static const appLogo = '$_imagesPath/movies_app_logo.png';
+
+  static const icSearchMovie = '$_iconsPath/ic_search_movie.svg';
+  static const icSearchEmpty = '$_iconsPath/ic_search_empty.svg';
 }
